604b51887b4264449350a1c0b8a83fa28e915796
FedyBenMahfoudh <fedi.benmahfoudh@isimm.u-monastir.tn>
2025-11-06 21:29:46 +0100

    Add main components: App, MovieCard, Search, Spinner, and Movie type definition

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
 function App() {
-  return <h1 className="text-9xl text-red-500">Hello world</h1>;
+
+  return (
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+
+          {/* Search Bar */}
+        </header>
+
+
+        <section className="all-movies">
+          <h2>All Movies</h2>
+
+          {/* Movie List */}
+
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
